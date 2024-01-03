c61eebd13162cce1930a8d16f1c0e5f19e1d0b9d
Ramya Tammisetti <ramya@Ramyas-MBP.attlocal.net>
2024-01-03 12:30:00 -0800

    added brief and titles to charts

--- streamlit_graph.py
+++ streamlit_graph.py
@@ -27,6 +27,18 @@ if 'MonthYear' not in st.session_state:
     month_year_list = db_ops.get_month_year_timeline(client, database_name, collection_name1)
     st.session_state['MonthYear'] = month_year_list
 
+#Header and brief about the functionality of the program
+'''
+# COVID over the United States
+
+Use the widgets below to select the data inputs - states, time (by month and year), and the type of data to view 
+
+Generate your output!
+
+A line graph (or graphs) will be displayed comparing up to 10 states for the timeline suggested. 
+Each line graph will compare the selected states for the data type selected, eg. the number of cases.
+'''
+
 with st.form("inputs"):
     selected_states = st.multiselect('Select State(s)', st.session_state['AllState'], default=[st.session_state['AllState'][0]], max_selections=10)
     date_range = st.select_slider('Select Date Range', options=st.session_state['MonthYear'], value=(st.session_state['MonthYear'][0], st.session_state['MonthYear'][-1]))
@@ -48,25 +60,41 @@ def create_plot(selected_states, start_date, end_date, data_types):
     cases_chart = base.mark_line().encode(
         y=alt.Y('Total_Cases:Q', axis=alt.Axis(title='Cases')),
         color='State:N'  # Encode color by State
+    ).properties(
+        title=alt.TitleParams(
+            text='Number of Cases per Month'
+        )
     )
 
     # Deaths line chart
     deaths_chart = base.mark_line().encode(
         y=alt.Y('Total_Deaths:Q', axis=alt.Axis(title='Deaths')),
         color='State:N'  # Encode color by State
+    ).properties(
+        title=alt.TitleParams(
+            text='Number of Deaths per Month'
+        )
     )
 
     # Cases/100k line chart
     cases100k_chart = base.mark_line().encode(
         y=alt.Y('Cases_per_100k:Q', axis=alt.Axis(title='Cases per 100k')),
         color='State:N'  # Encode color by State
+    ).properties(
+        title=alt.TitleParams(
+            text='Number of Cases per 100k per Month'        
+        )
     )
 
-    # Deaths line chart
+    # Deaths/100k line chart
     deaths100k_chart = base.mark_line().encode(
         y=alt.Y('Deaths_per_100k:Q', axis=alt.Axis(title='Deaths per 100k')),
         color='State:N'  # Encode color by State
+    ).properties(
+        title=alt.TitleParams(
+            text='Number of Deaths per 100k per Month'        )
     )
+
     # Filter based on selected data types
     if len(selected_states) == 1 and 'cases per 100k' in data_types and 'deaths per 100k' in data_types:
         
@@ -76,6 +104,10 @@ def create_plot(selected_states, start_date, end_date, data_types):
             as_=['Category', 'Value']
         )
 
+        # Construct the dynamic title
+        states_title = ', '.join(selected_states)
+        chart_title = f'COVID-19 Data per 100k for {states_title}'  # Dynamic title
+
         # Create line chart
         chart = folded_data.mark_line().encode(
             y=alt.Y('Value:Q', axis=alt.Axis(title='Count per 100k')),
@@ -83,7 +115,11 @@ def create_plot(selected_states, start_date, end_date, data_types):
             tooltip=['Category:N', 'Value:Q']
         ).resolve_scale(
             y='independent'
+        ).properties(
+        title=alt.TitleParams(
+            text=chart_title
         )
+    )
         
         st.altair_chart(chart, use_container_width=True)
 
